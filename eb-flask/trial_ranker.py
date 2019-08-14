20439ec02aa1577f3438fdcd7d8bc8502c53795a
Connor <connorpheraty@gmail.com>
2019-08-13 20:31:29 -0500

    Added docstrings to each function

--- eb-flask/trial_ranker.py
+++ eb-flask/trial_ranker.py
@@ -1,9 +1,13 @@
 import pandas as pd 
 from collections import Counter
 
+# Instantiate DataFrame from cleaned csv 
 df = pd.read_csv('Final.csv')
 
 def token_counts(string, search):
+  """
+  Function that counts the number of times a user search term is mentioned in a given tokenized list
+  """
     counter = Counter(string.strip('[],"').split(','))
     count = []
     for key, val in counter.items():
@@ -12,20 +16,18 @@ def token_counts(string, search):
     return sum(count)    
 
 def trial_ranker(user_search):
+  """
+  Function that ranks each trial using the token_counts function
+  """
 
   df = pd.read_csv('Final.csv')
-  print(user_search)
-
   df['counts'] = df['tokens'].apply(lambda x: token_counts(x, user_search))
   
-
+  # Sort dataframe by counts column descending from highest to lowest
   df_sorted = df.sort_values(by=['counts'], ascending=False)
-  print(df_sorted['counts'])
-  print(df_sorted['counts'].value_counts())
-
   df_sorted = df_sorted.drop('counts', axis=1)
 
-
+  # Return top 15 results
   df_sorted = df_sorted.head(15)
 
   return df_sorted
